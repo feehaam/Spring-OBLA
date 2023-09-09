252f25c859ead5d40bd32ce5e371640a8f0e5afb
feehaam <mdfeeham@gmail.com>
2023-09-09 23:43:14 +0600

    User DTO included

---
+++ src/main/java/com/feeham/obla/model/UserReadDTO.java
@@ -0,0 +1,16 @@
+package com.feeham.obla.model;
+import com.feeham.obla.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserReadDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private Role role;
+}
---
+++ src/main/java/com/feeham/obla/model/UserUpdateDTO.java
@@ -0,0 +1,17 @@
+package com.feeham.obla.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserUpdateDTO {
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String address;
+    private String role;
+}
