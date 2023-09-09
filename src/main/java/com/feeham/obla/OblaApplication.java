b8a1239df4a67a0ba002e7bdf762ae2a5ca46546
feehaam <mdfeeham@gmail.com>
2023-09-09 17:14:29 +0600

    Initial setup

---
+++ src/main/java/com/feeham/obla/OblaApplication.java
@@ -0,0 +1,13 @@
+package com.feeham.obla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OblaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OblaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feeham/obla/OblaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feeham.obla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OblaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
